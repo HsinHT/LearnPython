ba84474847ce01bb2edbbca0774e3d42996464e0
Tyr <tiw135@gmail.com>
2018-12-21 00:12:16 +0800

    fix bug

--- guess.py
+++ guess.py
@@ -1,9 +1,20 @@
+import random
+
+secret = random.randint(1,10)
+flag = True
+
 print('----------------------------------------')
-temp = input("guess number (0-9): ")
-guess = int(temp)
-if guess == 8:
-	print("guess right")
-else:
-	print("guess wrong")
+while flag:
+        temp = input("guess number (0-9): ")
+        guess = int(temp)
+
+        if guess == secret:
+                print("guess right")
+                flag = False
+        else:
+                if guess > secret:
+                        print("guess number too big")
+                else:
+                        print("guess number too small")
 print('----------------------------------------')
 print("Game end")
