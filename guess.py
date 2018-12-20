fadc941f173899a28713992cf2f46f4ae9d00f19
Tyr <tiw135@gmail.com>
2018-12-20 23:36:41 +0800

    fix bug

--- guess.py
+++ guess.py
@@ -1,9 +1,9 @@
-print('--------------------')
-temp = input("guess number (0-9):")
+print('----------------------------------------')
+temp = input("guess number (0-9): ")
 guess = int(temp)
-if guess == 8
+if guess == 8:
 	print("guess right")
-else
+else:
 	print("guess wrong")
-
+print('----------------------------------------')
 print("Game end")
