cb3fe9e9fc21bde944fba5f1f6faeb9eb90a3e2c
Tyr <tiw135@gmail.com>
2018-12-22 23:43:37 +0800

    add example factorial

---
+++ factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+	if n == 1:
+		return 1
+	else:
+		return n * factorial(n-1)
+
+number = int(input('Please input number: '))
+result = factorial(number)
+print("%d : %d" % (number, result))
